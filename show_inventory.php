9ce4a97973b7e8e59b104084a35a9e4adf1ffd7a
Baptiste Williams <tc1028@hotmail.com>
2025-07-06 21:03:11 -0700

    Cleaned up spacing in PHP file to make it easier to read

--- show_inventory.php
+++ show_inventory.php
@@ -3,10 +3,16 @@
 // show_inventory.php
 
 // Reflection:
+
+// 
 // I picked these items because they’re things I actually use every day—like my backpack, laptop, iPhone chargers, wallet, and my work uniform.
+// 
 // I even tested the system by changing the quantity of my laptop in phpMyAdmin, and it updated right away when I refreshed the page. That was really cool to see—it felt like I was in control of my own database.
+// 
 // If this was a real inventory system, it could be used by stores or workplaces to keep track of stuff people use or borrow.
+// 
 // Using PDO is helpful because it keeps the database safe from hackers trying to mess with it using fake input.
+// 
 // Updated Reflection
 
 try {
